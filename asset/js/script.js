a1887f5d3e2bb083e4a47cc40dbf687aed795ce5
a.rahimi <rahimi.a.2007@gmail.com>
2026-08-08 00:20:17 +0330

    debug Mobile Nav Menu

--- asset/js/script.js
+++ asset/js/script.js
@@ -7,6 +7,8 @@ menuToggle.addEventListener("click", function () {
     mobileNav.classList.add("opacity-100");
     mobileNav.classList.remove("h-0");
     mobileNav.classList.add("h-[180px]");
+    mobileNav.classList.remove("-translate-y-52");
+    mobileNav.classList.add("-translate-x-0");
 
     menuToggle.innerHTML = "";
     menuToggle.innerHTML = `<i data-lucide="x" class="w-4 h-4"></i>`;
@@ -16,8 +18,9 @@ menuToggle.addEventListener("click", function () {
   } else {
     mobileNav.classList.add("opacity-0");
     mobileNav.classList.remove("opacity-100");
-    mobileNav.classList.remove("h-[164px]");
+    mobileNav.classList.remove("h-[180px]");
     mobileNav.classList.add("h-0");
+    mobileNav.classList.add("-translate-y-52");
 
     menuToggle.innerHTML = "";
     menuToggle.innerHTML = `<i data-lucide="menu" class="w-4 h-4"></i> `;
