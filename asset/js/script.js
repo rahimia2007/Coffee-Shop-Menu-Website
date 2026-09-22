8f2dc385fa050ad1128803d5a916c4ed02fa0134
a.rahimi <rahimi.a.2007@gmail.com>
2026-09-23 01:26:18 +0330

    add week days handler function

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,8 +1,24 @@
+const dayEls = document.querySelectorAll("#day");
 
+const weekDaysHandeler = (() => {
+  const weekDaysName = [
+    "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+  ];
 
-const productCards = document.querySelectorAll("#product-card");
+  const today = new Date().getDay();
 
-
-productCards.forEach(function (productCard) {
-  productCard.addEventListener("click", showProductModal);
-});
+  dayEls.forEach((day) => {
+    if (day.innerHTML === weekDaysName[today]) {
+      day.innerHTML += `<span class="ml-2 text-xs font-dmmono opacity-60">(today)</span>`;
+      day.className = "text-sm font-medium text-primary";
+      const workHoursEls = day.nextElementSibling;
+      workHoursEls.className = "text-sm font-dmmono text-primary-text";
+    }
+  });
+})();
